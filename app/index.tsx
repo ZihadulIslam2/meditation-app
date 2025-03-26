d828304c3e5fdccaaeb83fb2be975ac8bf188919
zihadulislam2 <zihadul708@gmail.com>
2025-03-26 15:08:50 +0600

    setup the native wind. create custom button com.

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,42 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, ImageBackground,   } from 'react-native'
+import React from 'react'
+import beachImage from '@/assets/meditation-images/beach.webp'
+import { LinearGradient } from 'expo-linear-gradient'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {StatusBar} from 'expo-status-bar'
+import '../global.css'
+import CustomButton from '@/components/CustomButton'
 
-export default function HomeScreen() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
+    <View className="flex-1">
+      <ImageBackground
+        source={beachImage}
+        resizeMode="cover"
+        className="flex-1"
+      >
+        <LinearGradient
+          className="flex-1"
+          colors={['rgba(0, 0, 0, 0.4)', 'rgba(0, 0, 0, 0.8)']}
+        >
+          <SafeAreaView className="flex-1 px-1 justify-between">
+            <View>
+              <Text className="text-center text-white font-bold text-4xl">
+                Simple Meditation
+              </Text>
+              <Text className='text-center text-white text-regular text-2xl mt-3'>Simplify meditation for everyone.</Text>
+            </View>
+
+            <View>
+              <CustomButton onPress={()=>console.log("first")} title='Get Started'/>
+            </View>
+
+            <StatusBar style="light" />
+          </SafeAreaView>
+        </LinearGradient> 
+      </ImageBackground>
     </View>
-  
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-});
- 
+export default App
